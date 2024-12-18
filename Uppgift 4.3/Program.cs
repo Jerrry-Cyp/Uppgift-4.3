879fa75deb3b6c816a5b90905a488ddd1ffa7ca5
alsa07007 <alsa07007@E-PW05CGR5>
2024-12-18 11:55:32 +0100

    Add project files.

---
+++ Uppgift 4.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x;
+        for (x = 1; x < 100; x++)
+        {
+            String FizzBuss = "FizzBuss";
+            String Fizz = "Fizz";
+            String Buzz = "Buzz";
+            
+            if (x % 3 == 0 && x % 5 != 0)
+            {
+                Console.WriteLine(Fizz);
+            }
+            if (x % 5 == 0 && x % 3 != 0)
+            {
+                Console.WriteLine(Buzz);
+            }
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine(FizzBuss);
+            }
+            else Console.WriteLine(x);
+        }
+    }
+}
